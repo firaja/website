483df07af7f0437a906eabdd67e7fab495fe371d
David Bertoldi (openmind) <david.bertoldi@openmindonline.it>
2021-11-19 17:33:10 +0100

    v4

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
 
   const data = {
     title: 'David Bertoldi | Technical Architect',
-    description: 'Senior Technical Architect at Salesforce. 6+ years of experience on Ecommerce with SAP Hybris/Commerce Cloud and Salesforce Commerce Cloud. Passionate about computer science, cloud computing, machine learning, mathematics and based in Milan, Italy',
+    description: 'Senior Technical Architect at Salesforce. 6+ years of experience on Ecommerce with SAP Hybris/Commerce Cloud and Salesforce Commerce Cloud. Passionate about Computer Science, Cloud Computing, Machine Learning and Mathematics. Based in Milan, Italy',
     year: new Date().getFullYear()
   };
 
